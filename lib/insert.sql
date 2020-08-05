bcb8afaf35dde808ce7189b855b9bab0e035e7df
Carter Parrett <cparrett94@hotmail.com>
2020-08-05 00:37:59 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -33,3 +33,21 @@ VALUES
 ("Author 1"),
 ("Author 2");
 
+INSERT INTO character_books (character_id, book_id)
+VALUES
+(1, 1),
+(2, 1),
+(3, 1),
+(4, 1),
+(5, 1),
+(6, 1),
+(7, 2),
+(8, 2),
+(1, 3),
+(2, 3),
+(3, 3),
+(4, 3),
+(5, 3),
+(6, 3),
+(7, 3),
+(8, 3);
